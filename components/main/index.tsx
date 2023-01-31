0d9188ba302d42bcfba03a31b48e6f44da5bf08d
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-31 19:17:15 -0300

    share

--- components/main/index.tsx
+++ components/main/index.tsx
@@ -1,15 +1,17 @@
 import { useQRCode } from "next-qrcode";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../input";
 import Select, { PropsOption } from "../select";
-
+import { EmailIcon, EmailShareButton, FacebookIcon, FacebookShareButton, WhatsappIcon, WhatsappShareButton } from "react-share";
+import Link from "next/link";
 export default function Main() {
     const { SVG } = useQRCode();
     const [text, setText] = useState("");
     const [size, setSize] = useState<number>(300);
     const [margin, setMargin] = useState<number>(0);
-    const [colorOne, setColorOne] = useState("#000000");
-    const [colorTwo, setColorTwo] = useState("#ffffff");
+    const [url, setUrl] = useState("");
+    const [colorOne, setColorOne] = useState<string>('#000000');
+    const [colorTwo, setColorTwo] = useState<string>('#ffffff');
     const options: PropsOption[] = [
         {
             value: 200,
@@ -21,6 +23,15 @@ export default function Main() {
         }
     ]
 
+    useEffect(() => {
+        if (window) {
+            setUrl(window.location.href);
+        }
+    }, [])
+
+    const getUrlShare = () => {
+        return `${url}share?data=${text}&size=${size}&dark=${colorOne.replace("#", '')}&light=${colorTwo.replace("#", '')}&margin=${margin}`;
+    }
     return (
         <main className='flex justify-center items-center py-20'>
             <div className='p-10 rounded-md border border-slate-300 md:w-1/2 text-black'>
@@ -42,8 +53,8 @@ export default function Main() {
                 </div>
                 <div className="md:mt-0 mt-10">
                     <h1 className="font-light text-3xl border-b border-slate-300 mb-5">Mude as cores</h1>
-                    <Input id={"color1"} textLabel={"#1"} type={"color"} className={"bg-white"} value={`${colorOne}`} onChange={(e) => { setColorOne(e.target.value) }} />
-                    <Input id={"color2"} textLabel={"#2"} type={"color"} className={"bg-white"} value={`${colorTwo}`} onChange={(e) => { setColorTwo(e.target.value) }} />
+                    <Input id={"color1"} textLabel={"#1"} type={"color"} className={"bg-white"} value={colorOne} onChange={(e) => { setColorOne(e.target.value) }} />
+                    <Input id={"color2"} textLabel={"#2"} type={"color"} className={"bg-white"} value={colorTwo} onChange={(e) => { setColorTwo(e.target.value) }} />
                 </div>
                 <div>
                     <h1 className="font-light text-3xl border-b border-slate-300 mb-5">Qrcode</h1>
@@ -68,9 +79,39 @@ export default function Main() {
                             </div>
                         </div>
                     }
+                    <h1 className="font-light text-2xl border-b mt-5 border-slate-300 mb-5">Compartilhar em</h1>
+                    <div className="flex flex-col md:flex-row mt-5">
+                        <WhatsappShareButton url={getUrlShare()}>
+                            <div className="flex bg-green-500 p-2 px-5 md:mr-5 rounded-md items-center justify-between text-white">
+                                <span className="mr-2">Whatsapp</span>
+                                <WhatsappIcon size={32} round />
+                            </div>
+                        </WhatsappShareButton>
+                        <FacebookShareButton url={getUrlShare()}>
+                            <div className="flex bg-blue-500 p-2 px-5 md:mr-5 md:mt-0 mt-2 justify-between  rounded-md items-center text-white">
+                                <span className="mr-2">Facebook</span>
+                                <FacebookIcon size={32} round />
+                            </div>
+                        </FacebookShareButton>
+                    </div>
                 </div>
-
             </div>
         </main>
     )
 }
+
+/**
+ * 
+ * <Link href={getUrlShare()} className={"flex items-center mt-5 p-2 px-5 bg-blue-500 text-white rounded-md hover:bg-blue-700 hover:transition-all w-fit"}>
+                        <span className="mr-2">Visualizar</span>
+                    </Link>
+ */
+
+/**
+ * <EmailShareButton url={getUrlShare()}>
+        <div className="flex bg-gray-500 p-2 px-5 md:mr-5 md:mt-0 mt-2 justify-between rounded-md items-center text-white">
+            <span className="mr-2">Email</span>
+            <EmailIcon size={32} round />
+        </div>
+    </EmailShareButton>
+ */
---
+++ pages/share.tsx
@@ -0,0 +1,59 @@
+import LayoutDefault from '@/layouts/default'
+import { GetServerSidePropsResult } from 'next';
+import { useQRCode } from 'next-qrcode';
+import { useEffect, useState } from 'react';
+
+interface IPropsShare {
+    data: string
+    size: number
+    dark: string
+    light: string
+    margin: number
+}
+
+
+export default function Share(props: IPropsShare) {
+    const { SVG } = useQRCode();
+    const { data, size, dark, light, margin } = props;
+    return (
+        <LayoutDefault titlePage='Share'>
+            <div className='flex flex-col items-center justify-center mt-10'>
+                <SVG
+                    text={data}
+                    options={{
+                        level: 'M',
+                        margin: margin ?? 3,
+                        scale: 2,
+                        width: size,
+                        color: {
+                            dark,
+                            light
+                        }
+                    }}
+                />
+            </div>
+        </LayoutDefault>
+    )
+}
+
+export async function getServerSideProps(context: any): Promise<GetServerSidePropsResult<IPropsShare>> {
+    const { data, size, dark, light, margin } = context.query;
+
+    if (data && dark && light && size && margin) {
+        return {
+            props: {
+                data,
+                size: parseInt(size),
+                dark,
+                light,
+                margin: parseInt(margin)
+            },
+        }
+    }
+    return {
+        redirect: {
+            destination: '/',
+            permanent: false
+        }
+    }
+}
